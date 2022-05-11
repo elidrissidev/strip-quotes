cbe484ecbeb21592b4013497551ad7a2e712f2d1
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-05-11 22:03:01 +0100

    Add function implementation

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function stripQuotes() {   
+/**
+ * Remove quote character surrounding a string
+ *
+ * @param {string} str A string with surrounding quotes
+ * @returns {string} `str` without surrounding quotes
+ */
+function stripQuotes(str) {
+  // Fail if an invalid argument is provided
+  if (typeof str !== 'string') {
+    throw new TypeError('Expected a string');
+  }
+  return str.replace(/^"(.+)"$/, '$1');
 }
 
 module.exports = stripQuotes;
